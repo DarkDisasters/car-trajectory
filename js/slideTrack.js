601c5e8e8c7fde996eecd6c2cec2de779e517df6
DarkDisaters <2361629815@qq.com>
2019-06-14 11:23:05 +0800

    1

--- js/slideTrack.js
+++ js/slideTrack.js
@@ -2,27 +2,30 @@ function transferRange(){
 
     var Mode = 0
     var Down = 0
+    var timeNow = "2010-04-06 00:00:00"
 
-    var slider = d3.select("#timerange")
-        .on("mousedown", function(){
-            Down = 1
-        })
-        .on("mouseup", function(){
-            Down = 0
-        })
-        .on("mousemove", function(){
-            var timeNumber = $("#timerange").val()
-            var time_day = Math.floor(timeNumber / 17280)
-            var time_hour = Math.floor((timeNumber-time_day*17280) / 720)
-            var time_minute = Math.floor((timeNumber-time_day*17280-time_hour*720) / 12)
-            var time_second = timeNumber-time_day*17280-time_hour*720-time_minute*12
+    // var slider = d3.select("#timerange")
+    //     .on("mousedown", function(){
+    //         Down = 1
+    //     })
+    //     .on("mouseup", function(){
+    //         Down = 0
+    //     })
+    //     .on("mousemove", function(){
+    //         var timeNumber = $("#timerange").val()
+    //         var time_day = Math.floor(timeNumber / 17280)
+    //         var time_hour = Math.floor((timeNumber-time_day*17280) / 720)
+    //         var time_minute = Math.floor((timeNumber-time_day*17280-time_hour*720) / 12)
+    //         var time_second = timeNumber-time_day*17280-time_hour*720-time_minute*12
 
-            console.log("timenow", "2010-04-"+transfer(time_day+6)+ " " + transfer(time_hour) + ":" + transfer(time_minute) + ":" + transfer(time_second))
-            if(Mode == 0 && Down == 1){
-                if(time_minute)
-                $('#time').val("2010-04-"+transfer(time_day+6)+ " " + transfer(time_hour) + ":" + transfer(time_minute) + ":" + transfer(time_second))
-            }
-        })
+    //         var timeNow = "2010-04-"+transfer(time_day+6)+ " " + transfer(time_hour) + ":" + transfer(time_minute) + ":" + transfer(time_second)
+
+    //         console.log("timenow", "2010-04-"+transfer(time_day+6)+ " " + transfer(time_hour) + ":" + transfer(time_minute) + ":" + transfer(time_second))
+    //         if(Mode == 0 && Down == 1){
+    //             $('#time').val(timeNow)
+    //             judgeStore()
+    //         }
+    //     })
 
     d3.csv('../csvdata/mobile/mobileSensor.csv', function(error,data){
         if(error){
@@ -30,8 +33,10 @@ function transferRange(){
         }
         else{
             clearall()
+            console.log("data", data)
             var areaDotList = []
             var totalValue = 0
+            var averageValue = 0
             var containerSvgTrack = document.getElementById('trajectory')
         //     console.log("height is ---" + containerSvgTrack.attributes.height.value +
         // "--- width is ---" + containerSvgTrack.attributes.width.value + "---")
@@ -62,25 +67,58 @@ function transferRange(){
                 var xend = j2*areawidth
                 var xwidth = j2*areawidth - xstart
             }
-            for (var i = 0; i <data.length; i++){
-                var longitude = parseFloat(lon2x(data[i].Long))
-                var latitude = parseFloat(lat2y(data[i].Lat))
-                var value = parseFloat(data[i].Value)
-                var timestamp = data[i].Timestamp
-                var datadict = {}
-                var lonplat = {}
-                
-                if (longitude < xend && longitude >= xstart && latitude < yend && latitude >= ystart){
-                    totalValue += value
-                    lonplat['x'] = longitude
-                    lonplat['y'] = latitude
-                    datadict['Value'] = value
-                    datadict['Timestamp'] = timestamp
-                    datadict['lonplat'] = lonplat
-                    areaDotList.push(datadict)
+            console.log("Start")
+            var slider = d3.select("#timerange")
+                .on("mousedown", function(){
+                    Down = 1
+                })
+                .on("mouseup", function(){
+                    Down = 0
+                })
+                .on("mousemove", function(){
+                    var timeNumber = $("#timerange").val()
+                    var time_day = Math.floor(timeNumber / 17280)
+                    var time_hour = Math.floor((timeNumber-time_day*17280) / 720)
+                    var time_minute = Math.floor((timeNumber-time_day*17280-time_hour*720) / 12)
+                    var time_second = timeNumber-time_day*17280-time_hour*720-time_minute*12
+
+                    var timeNow = "2010-04-"+transfer(time_day+6)+ " " + transfer(time_hour) + ":" + transfer(time_minute) + ":" + transfer(time_second)
+
+                    console.log("timenow", "2010-04-"+transfer(time_day+6)+ " " + transfer(time_hour) + ":" + transfer(time_minute) + ":" + transfer(time_second))
+                    if(Mode == 0 && Down == 1){
+                        $('#time').val(timeNow)
+                        judgeStore(timeNumber)
+                    }
+                })
+            
+
+            function judgeStore(number){
+                for (var i = 0; i <data.length; i++){
+                    var longitude = parseFloat(lon2x(data[i].Long))
+                    var latitude = parseFloat(lat2y(data[i].Lat))
+                    var value = parseFloat(data[i].Value)
+                    var timestamp = data[i].Timestamp
+                    var datadict = {}
+                    var lonplat = {}
+                    
+                    if (longitude < xend && longitude >= xstart && latitude < yend && latitude >= ystart && timestamp == timeNow){
+                        totalValue += value
+                        lonplat['x'] = longitude
+                        lonplat['y'] = latitude
+                        datadict['Value'] = value
+                        datadict['Timestamp'] = timestamp
+                        datadict['lonplat'] = lonplat
+                        areaDotList.push(datadict)
+                    }
                 }
+                averageValue = totalValue / areaDotList.length
+                console.log("hei", yheight)
+                console.log("wid", xwidth)
+                console.log("areaDotList", areaDotList)
+                console.log("totalValue", totalValue)
+                console.log("averageValue", averageValue)
             }
-            var averageValue = totalValue / areaDotList.length
+
             // if(i == 199){
             //     var yend = totalHeight
             //     var yheight = totalHeight - 199*areaheight
@@ -98,32 +136,31 @@ function transferRange(){
             //     var xend = (j+1)*areawidth
             //     var xwidth = areawidth
             // }
-            console.log("hei", yheight)
-            console.log("wid", xwidth)
-            console.log("areaDotList", areaDotList)
-            console.log("totalValue", totalValue)
-            console.log("averageValue", averageValue)
-            var gArea = svg.append("g")
+            function draw(){
+                var gArea = svg.append("g")
                     .attr("class", "Area(" + String(j) + "," + String(i) + ")")
 
-            var contour = gArea.append("rect")
-                    .attr("x", xstart)
-                    .attr("y", ystart)
-                    .attr("width", xwidth)
-                    .attr("height", yheight)
-                    .style("stroke", "#F27219")
-                    .style("stroke-width",2)
-                    .style("fill","none")
+                var contour = gArea.append("rect")
+                        .attr("x", xstart)
+                        .attr("y", ystart)
+                        .attr("width", xwidth)
+                        .attr("height", yheight)
+                        .style("stroke", "#F27219")
+                        .style("stroke-width",2)
+                        .style("fill","none")
 
-            var dotlist = gArea.append("g")
-                    .attr("class","AreaDot")
-            for (var j = 0; j < areaDotList.length; j++){
-                dotlist.append("circle")
-                    // .attr("r", radiusscale(areaDotList[j]['Value']))
-                    .attr("r",4)
-                    .attr("transform", "translate(" + areaDotList[j]['lonplat']['x'] + ',' + areaDotList[j]['lonplat']['y'] + ")")
-                    .style("fill", "#F27219")
+                var dotlist = gArea.append("g")
+                        .attr("class","AreaDot")
+                for (var j = 0; j < areaDotList.length; j++){
+                    dotlist.append("circle")
+                        // .attr("r", radiusscale(areaDotList[j]['Value']))
+                        .attr("r",4)
+                        .attr("transform", "translate(" + areaDotList[j]['lonplat']['x'] + ',' + areaDotList[j]['lonplat']['y'] + ")")
+                        .style("fill", "#F27219")
+                }
+                $('#meanvalue').val(averageValue)
             }
+            
         }
     })
 }
